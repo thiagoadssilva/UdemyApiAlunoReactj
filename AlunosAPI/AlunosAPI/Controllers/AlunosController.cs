1caa606b084fdb9adc58e399f713e46fb93c8f25
Thiago jose <thiago.ads.silva@gmail.com>
2022-07-28 20:47:32 -0300

    Criação do serviços e iniciando o controlador

---
+++ AlunosAPI/AlunosAPI/Controllers/AlunosController.cs
@@ -0,0 +1,18 @@
+﻿using AlunosAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosController : ControllerBase
+    {
+        private IAlunoService _alunoService;
+
+        public AlunosController(IAlunoService alunoService)
+        {
+            _alunoService = alunoService;
+        }
+    }
+}
