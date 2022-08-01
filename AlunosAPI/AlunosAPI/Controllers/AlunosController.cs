e2e3c5c214d1ca8c3c360fb948b3c25651732d32
Thiago jose <thiago.ads.silva@gmail.com>
2022-08-01 11:01:09 -0300

    Finalizando API de alunos

--- AlunosAPI/AlunosAPI/Controllers/AlunosController.cs
+++ AlunosAPI/AlunosAPI/Controllers/AlunosController.cs
@@ -29,5 +29,105 @@ namespace AlunosAPI.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
             }
         }
+
+        [HttpGet("AlunoPorNome")]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunoByName([FromQuery] String nome)
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunosByNome(nome);
+                if (alunos.Count() == 0) {
+                    return NotFound($"Não existe aluno com o critéio {nome}");
+                }
+                else
+                {
+                    return Ok(alunos);
+                }
+                
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+            }
+        }
+
+        [HttpGet("{id:int}", Name = "GetAluno")]
+        public async Task<ActionResult<Aluno>> GetAluno(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+                if (aluno == null)
+                {
+                    return NotFound($"Não existe aluno com o critéio {id}");
+                }
+                else
+                {
+                    return Ok(aluno);
+                }
+
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Aluno aluno)
+        {
+            try
+            {
+                await _alunoService.CreateAluno(aluno);
+                return CreatedAtRoute(nameof(GetAluno), new { id = aluno.Id }, aluno);
+            }
+            catch
+            {
+                return BadRequest("Request Invalido");
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Edit(int id, [FromBody] Aluno aluno)
+        {
+            try
+            {
+                if (aluno.Id == id)
+                {
+                    await _alunoService.UpdateAluno(aluno);
+                    return Ok($"Aluno com id={id} foi atualizado com sucesso");
+                }
+                else
+                {
+                    return BadRequest("Dados Inconsistente");
+                }
+            }
+            catch
+            {
+
+                return BadRequest("Request Invalido");
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+                if (aluno != null) {
+                    await _alunoService.DeleteAluno(aluno);
+                    return Ok($"Aluno com id={id} foi excluido com sucesso");
+                }
+                else
+                {
+                    return NotFound($"Aluno com id={id} não foi encontrado");
+                }
+            }
+            catch
+            {
+                return BadRequest("Request Invalido");
+            }
+        }
     }
 }
