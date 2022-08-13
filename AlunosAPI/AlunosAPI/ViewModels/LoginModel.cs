bb84717d80a02f978ad8f8f4ec3ed6e891833887
Thiago jose <thiago.ads.silva@gmail.com>
2022-08-13 07:34:08 -0300

    Correção na autenticação do aluno(Login)

--- AlunosAPI/AlunosAPI/ViewModels/LoginModel.cs
+++ AlunosAPI/AlunosAPI/ViewModels/LoginModel.cs
@@ -9,7 +9,7 @@ namespace AlunosAPI.ViewModels
         public string Email { get; set; }
 
         [Required(ErrorMessage = "A senha é obrigatório")]
-        [StringLength(20, ErrorMessage = " A {0} deve ter no mínimo {2} e no máximo {10}.", MinimumLength = 10)]
+        [StringLength(20, ErrorMessage = " A {0} deve ter no mínimo {2} e no máximo {10}.", MinimumLength = 1)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
